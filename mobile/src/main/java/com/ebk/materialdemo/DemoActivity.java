9a3187e16cc06b29b23671e08779ac4012a9ed61
Yasin Yildirim <yyildirim@team.mobile.de>
2014-10-25 13:29:40 +0200

    Add swipe refresh

--- mobile/src/main/java/com/ebk/materialdemo/DemoActivity.java
+++ mobile/src/main/java/com/ebk/materialdemo/DemoActivity.java
@@ -1,6 +1,8 @@
 package com.ebk.materialdemo;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,20 +14,50 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
+
 public class DemoActivity extends ActionBarActivity {
+
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setColorSchemeResources(
+                android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_purple);
+        swipeRefreshLayout.setOnRefreshListener(new RefreshListener());
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(layoutManager);
 
-        recyclerView.setAdapter(new RecyclerAdapter(new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}));
+        populateList();
+    }
+
+    private void populateList() {
+        recyclerView.setAdapter(new RecyclerAdapter(getSortedCountryList()));
+    }
+
+    private ArrayList<String> getSortedCountryList() {
+        Locale[] locales = Locale.getAvailableLocales();
+        ArrayList<String> countries = new ArrayList<String>();
+        for (Locale locale : locales) {
+            String country = locale.getDisplayCountry();
+            if (country.trim().length() > 0 && !countries.contains(country)) {
+                countries.add(country);
+            }
+        }
+        Collections.sort(countries);
+        return countries;
     }
 
     @Override
@@ -48,9 +80,9 @@ public class DemoActivity extends ActionBarActivity {
     }
 
     private static class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
-        private String[] dataset;
+        private ArrayList<String> dataset;
 
-        public RecyclerAdapter(String[] myDataset) {
+        public RecyclerAdapter(ArrayList<String> myDataset) {
             dataset = myDataset;
         }
 
@@ -65,12 +97,12 @@ public class DemoActivity extends ActionBarActivity {
 
         @Override
         public void onBindViewHolder(ViewHolder holder, int position) {
-            holder.textView.setText(dataset[position]);
+            holder.textView.setText(dataset.get(position));
         }
 
         @Override
         public int getItemCount() {
-            return dataset.length;
+            return dataset.size();
         }
 
         public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -83,4 +115,16 @@ public class DemoActivity extends ActionBarActivity {
         }
     }
 
+    private class RefreshListener implements SwipeRefreshLayout.OnRefreshListener {
+        @Override
+        public void onRefresh() {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    populateList();
+                    swipeRefreshLayout.setRefreshing(false);
+                }
+            }, 5000); //adding five sec delay
+        }
+    }
 }
