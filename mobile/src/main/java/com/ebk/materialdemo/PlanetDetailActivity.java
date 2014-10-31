660b2987fc412b4e8ceae7006e39776c84e6a142
Yasin Yildirim <yyildirim@team.mobile.de>
2014-10-31 14:09:20 +0100

    Add activity transition example with shared elements

---
+++ mobile/src/main/java/com/ebk/materialdemo/PlanetDetailActivity.java
@@ -0,0 +1,56 @@
+package com.ebk.materialdemo;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+
+public class PlanetDetailActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planet_detail);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ImageView image = (ImageView) findViewById(R.id.planet_image);
+        ViewCompat.setTransitionName(image, PlanetsActivity.EXTRA_IMAGE);
+        Picasso.with(this).load(getIntent().getIntExtra(PlanetsActivity.EXTRA_IMAGE, R.drawable.earth)).into(image);
+
+        TextView name = (TextView) findViewById(R.id.planet_name);
+        ViewCompat.setTransitionName(name, PlanetsActivity.EXTRA_NAME);
+        name.setText(getIntent().getStringExtra(PlanetsActivity.EXTRA_NAME));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.planet_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- mobile/src/main/java/com/ebk/materialdemo/PlanetsActivity.java
+++ mobile/src/main/java/com/ebk/materialdemo/PlanetsActivity.java
@@ -1,17 +1,51 @@
 package com.ebk.materialdemo;
 
-import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 
 public class PlanetsActivity extends BaseNavigationDrawerActivity {
+
+    public static final String EXTRA_IMAGE = "IMAGE";
+    public static final String EXTRA_NAME = "NAME";
+    private int[] imageResIds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setSelectedDrawerItemPosition(1);
+
+        imageResIds = new int[] {
+                R.drawable.mercury,
+                R.drawable.venus,
+                R.drawable.earth,
+                R.drawable.mars,
+                R.drawable.jupiter,
+                R.drawable.saturn,
+                R.drawable.uranus,
+                R.drawable.neptune,
+                R.drawable.pluto
+        };
+
+        GridView gridView = (GridView) findViewById(R.id.planets_grid);
+        gridView.setAdapter(new GridViewAdapter(this, imageResIds, getResources().getStringArray(R.array.planet_names)));
+        gridView.setOnItemClickListener(new GridItemClickListener());
     }
 
     @Override
@@ -41,5 +75,80 @@ public class PlanetsActivity extends BaseNavigationDrawerActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private static class GridViewAdapter extends BaseAdapter {
+
+        private final LayoutInflater inflater;
+        private Context context;
+        private final int[] imageResIds;
+        private final String[] names;
+
+        public GridViewAdapter(Context context, int[] imageResIds, String[] names) {
+            this.context = context;
+            this.imageResIds = imageResIds;
+            this.names = names;
+            inflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return imageResIds.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return "Item " + String.valueOf(position + 1);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder;
+
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.grid_item_planet, null);
+                holder = new ViewHolder();
+                holder.planetImage = (ImageView) convertView.findViewById(R.id.grid_item_planet_image);
+                holder.planetName = (TextView) convertView.findViewById(R.id.grid_item_planet_name);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            Picasso.with(context).load(imageResIds[position]).into(holder.planetImage);
+            holder.planetName.setText(names[position]);
+
+            return convertView;
+        }
+
+        private static class ViewHolder {
+            private ImageView planetImage;
+            private TextView planetName;
+        }
+
+    }
+
+    private class GridItemClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            // Define transition options
+            ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(PlanetsActivity.this,
+                    new Pair<View, String>(view.findViewById(R.id.grid_item_planet_image), EXTRA_IMAGE),
+                    new Pair<View, String>(view.findViewById(R.id.grid_item_planet_name), EXTRA_NAME));
+
+            // Create intent
+            Intent intent = new Intent(PlanetsActivity.this, PlanetDetailActivity.class);
+            intent.putExtra(EXTRA_IMAGE, imageResIds[position]);
+            intent.putExtra(EXTRA_NAME, getResources().getStringArray(R.array.planet_names)[position]);
+
+            // Start activity with defined options
+            ActivityCompat.startActivity(PlanetsActivity.this, intent, options.toBundle());
+        }
     }
 }
